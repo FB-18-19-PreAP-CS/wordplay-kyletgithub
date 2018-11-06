2e62fc2697373c80305b1ac0efa4cbdb9c6013ae
THREATT.KYLE <2200796@fbisd.flourbluffschools.net>
2018-11-06 14:13:05 -0600

    finished

---
+++ module_8.py
@@ -0,0 +1,6 @@
+for i in range(1000000):
+    if (str(i).zfill(6))[2:] == (str(i).zfill(6))[5:1:-1]:
+        if (str(i+1).zfill(6))[1:] == (str(i+1).zfill(6))[5:0:-1]:
+            if (str(i+2).zfill(6))[1:5] == (str(i+2).zfill(6))[4:0:-1]:
+                if (str(i+3).zfill(6)) == (str(i+3).zfill(6))[::-1]:
+                    print(i)
