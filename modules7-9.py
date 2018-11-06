4ef7f79980961e205907a91d7ce34d07a1c3e996
THREATT.KYLE <2200796@fbisd.flourbluffschools.net>
2018-11-06 13:41:39 -0600

    initial commit

---
+++ modules7-9.py
@@ -0,0 +1,11 @@
+def double_letters():
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.split():
+                for i in range(len(word)-5):
+                    if word[i] == word[i+1] and word[i+2] == word[i+3] and word[i+4] == word[i+5]:
+                        print(word)
+                        
+if __name__ == "__main__":
+    double_letters()
+    
